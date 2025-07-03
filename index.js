cd42f8ed3cec5524cd65b9cc3feab6b6b0833dcd
Guevaer Arnaud <guevaer8@gmail.com>
2025-07-03 18:12:31 +0200

    working jokes with json

---
+++ index.js
@@ -0,0 +1,20 @@
+let blagues = [];
+
+fetch("./jokes.json")
+  .then(response => response.json())
+  .then(data => {
+    blagues = data.jokes;
+  })
+  .catch(() => {
+    document.getElementById('joke').textContent = "Erreur lors du chargement des blagues.";
+  });
+
+document.getElementById('btnJoke').addEventListener('click', () => {
+  if (blagues.length === 0) {
+    document.getElementById('joke').textContent = "Chargement des blagues...";
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * blagues.length);
+  const blague = blagues[randomIndex];
+document.getElementById('joke').innerHTML = `${blague.question}<br>— ${blague.answer}`;
+});
